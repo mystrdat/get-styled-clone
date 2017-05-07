afcd2217d879016f922a684b5b18fb7591f3b7ef
mystrdat <mystrdat@gmail.com>
2017-05-07 05:24:34 +0200

    improvements

--- src/index.js
+++ src/index.js
@@ -1,40 +1,31 @@
-const getStyledClone = (elements, position) => {
-
-  const nodes = elements instanceof HTMLElement ? [elements] : Array.from(elements),
-        fragment = document.createDocumentFragment();
-
-  nodes.forEach((node, i) => {
-    const clone = node.cloneNode(true),
-          cloneChilren = Array.prototype.slice.call(clone.querySelectorAll('*')),
-          nodeChildren = Array.prototype.slice.call(node.querySelectorAll('*'));
-    
+const getStyledClone = (element, position = false) => {
+  if (element instanceof HTMLElement) {
+    const clone = element.cloneNode(true),
+          cloneNodes = Array.prototype.slice.call(clone.querySelectorAll('*')),
+          elementNodes = Array.prototype.slice.call(element.querySelectorAll('*'));
     // Process parent
-    clone.style.cssText = window.getComputedStyle(node).cssText;
+    clone.style.cssText = window.getComputedStyle(element).cssText;
     clone.style.webkitTextFillColor = 'initial';
+    // Process children
+    cloneNodes.forEach((node, i) => {
+      node.style.cssText = window.getComputedStyle(elementNodes[i]).cssText;
+      node.style.webkitTextFillColor = 'initial';
+    })
+    // Set clone position
     if (position) {
-      const elementBounds = node.getBoundingClientRect();
-       // Positioning
+      const elementBounds = element.getBoundingClientRect();
       clone.style.position = 'fixed';
       clone.style.left = `${elementBounds.left}px`;
       clone.style.top = `${elementBounds.top}px`;
-      clone.style.zIndex = options.zIndex;
       // Conflicting properties
       clone.style.margin = 0;
-      clone.style.transform = 'none';
+      clone.style.transform = 'none'; // TODO: keep relevant transforms
     }
-    
-    // Process children
-    cloneChildren.forEach((child, n) => {
-      child.style.cssText = window.getComputedStyle(nodeChildren[n]).cssText;
-      child.style.webkitTextFillColor = 'initial';
-    })
-
     // Append to fragment
-    fragment.appendChild(clone);
-  })
-
-  // Return DOM fragment
-  return fragment;
+    return clone;
+  } else {
+    throw new Error('Parameter must be a HTMLElement reference.');
+  }
 }
 
 export default getStyledClone;
